85440828ab826e3e34b694799b9842ba237b1351
Guilherme <guilhermehenrique.oliv3@gmail.com>
2026-04-18 20:28:28 -0300

    fix: move typescript to dependencies, always compile on start

--- apps/api/start.sh
+++ apps/api/start.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 set -e
 
-if [ ! -f "dist/server.js" ]; then
-  echo "[Deploy] Compilando TypeScript..."
-  npx prisma generate --schema=./prisma/schema.prisma
-  npx tsc -p tsconfig.json
-fi
+echo "[Deploy] Compilando TypeScript..."
+npx prisma generate --schema=./prisma/schema.prisma
+npx tsc -p tsconfig.json
 
 echo "[Deploy] Rodando migrações..."
 npx prisma migrate deploy --schema=./prisma/schema.prisma
